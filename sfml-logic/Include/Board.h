ec5ca11e9a302bbe6697186a3cb6bd27412f8d7d
doruk gürkan <dorukgurkan1@gmail.com>
2022-05-23 10:31:03 +0300

    description

--- sfml-logic/Include/Board.h
+++ sfml-logic/Include/Board.h
@@ -10,9 +10,16 @@ public:
 	sf::RenderWindow* window;
 
 	Board(sf::RenderWindow* w);
+	//this could have been virtual too
 	void setBackGround(sf::Vector2f p, sf::Vector2f s,
 								sf::Color color);
+	//generic draw function
+	//All boards have different
+	//stuff they need to call draw on too
+	//so this needs to be overridden
 	virtual void draw() const;
+	//generic isInside function to check if
+	//mouse is inside given object
 	bool isInside(sf::Vector2f mp) const;
 };
 
--- sfml-logic/Include/Object.h
+++ sfml-logic/Include/Object.h
@@ -7,6 +7,13 @@
 class Object
 {
 public:
+	/*
+	Maybe this could have been inside just
+	Button class but early on we thought
+	LogicElement class would have taken advantage
+	of this enumeration too
+	That's why it's here
+	*/
 	enum class objectType {
 		AndGate,
 		OrGate,
@@ -23,12 +30,26 @@ public:
 		NoButton
 	};
 
+	//All object have sprites (except wire)
 	sf::Sprite sprite;
+	//All will have a window reference to draw
+	//themselves
 	sf::RenderWindow* window;
+	/*
+	Instead of every object locally loading
+	their own texture, all of them point to a
+	single Textures object to get their
+	own texture. Saves memory and gives much
+	more cleaner code
+	*/
 	static Textures* textures;
 //functions
 	Object(sf::RenderWindow* w);
+	//Since all object deriving classed will have
+	//their own draw method, I predeclared it here
 	virtual void draw() const;
+
+	//DEPRECATED WITH STATIC TEXTURES CLASS
 	//void setTexture(std::string str);
 };
 
